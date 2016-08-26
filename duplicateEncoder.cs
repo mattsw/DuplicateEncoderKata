565b5c83c0904baa7a44d021f083484f4ad78611
Matt Wilhelm <mattw@stcu.org>
2016-08-26 15:29:39 -0700

    Upgraded the c# solution

--- duplicateEncoder.cs
+++ duplicateEncoder.cs
@@ -5,18 +5,11 @@ using System.Collections.Generic;
 public class Kata
 {
 
-  public static string DuplicateEncode(string word)
-  {
-    var result = word.ToLower().ToCharArray().Aggregate(new Dictionary<string, int>(), (current, next) =>
-            {
-                var strVal = next.ToString();
-                if (current.ContainsKey(strVal)) current[strVal] = current[strVal] + 1;
-                else current.Add(strVal, 1);
-
-                return current;
-            });
-    return string.Join(string.Empty, word.ToLower()
-        .ToCharArray()
-        .Select(character => result[character.ToString()] == 1 ? "(" : ")"));
-  }
+  public static string DuplicateEncode(string word) => 
+            new string(word.ToLower()
+                .Select(character =>
+                    word.ToLower()
+                        .Count(innerCharacter => character == innerCharacter) == 1
+                        ? '('
+                        : ')').ToArray());
 }
